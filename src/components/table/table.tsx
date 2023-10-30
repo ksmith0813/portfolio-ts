052ccde125162653f9b2eb849eee368a1f1adf85
Kevin Smith <ksmith0813@gmail.com>
2023-10-30 16:10:33 -0500

    added static data for table grid, moved column defs and static data to the new data directory

--- src/components/table/table.tsx
+++ src/components/table/table.tsx
@@ -1,23 +1,24 @@
 import { Box } from '@mui/material'
 import { DataGrid } from '@mui/x-data-grid'
-import { useDemoData } from '@mui/x-data-grid-generator'
+import { columns } from 'data/table/columns'
+import { rows } from 'data/table/rows'
 
-export const Table = () => {
-  const { data } = useDemoData({
-    dataSet: 'Commodity',
-    rowLength: 1000,
-    editable: true,
-  })
-
-  return (
-    <Box sx={{ height: 'calc(100vh - 130px)', width: '100%' }}>
-      <DataGrid
-        {...data}
-        loading={data.rows.length === 0}
-        rowHeight={38}
-        checkboxSelection
-        disableRowSelectionOnClick
-      />
-    </Box>
-  )
-}
+export const Table = () => (
+  <Box sx={{ height: 'calc(100vh - 130px)', width: '100%' }}>
+    <DataGrid
+      loading={rows.length === 0}
+      rows={rows}
+      columns={columns}
+      initialState={{
+        pagination: {
+          paginationModel: {
+            pageSize: 20,
+          },
+        },
+      }}
+      pageSizeOptions={[20]}
+      checkboxSelection
+      disableRowSelectionOnClick
+    />
+  </Box>
+)
